e27c20ebed44e07d4142abf5df7939c37acb726d
ZJC <zjcnof@163.com>
2016-12-15 12:35:10 +0800

    修改TextView名字

--- app/src/main/java/com/zjc/testgithub/MainActivity.java
+++ app/src/main/java/com/zjc/testgithub/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
+    private TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
