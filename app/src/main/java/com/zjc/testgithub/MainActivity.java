9e142937c58bf6a99f44efc8249a046cec5f1c48
ZJC <zjcnof@163.com>
2016-12-15 12:08:54 +0800

    第一次提交

---
+++ app/src/main/java/com/zjc/testgithub/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zjc.testgithub;
+
+import android.media.tv.TvContentRating;
+import android.media.tv.TvView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
